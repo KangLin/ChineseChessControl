ab73850ed5ea0c1e15ba00adbe22ddf0e6c23cbe
KangLin <kl222@126.com>
2020-05-25 11:54:58 +0800

    Add desiger dll

---
+++ Src/Qt/FrmChineseChessPlugin.cpp
@@ -0,0 +1,74 @@
+#include "FrmChineseChess.h"
+#include "FrmChineseChessPlugin.h"
+
+#include <QtPlugin>
+#include <QIcon>
+
+CFrmChineseChessPlugin::CFrmChineseChessPlugin(QObject *parent)
+    : QObject(parent)
+{
+    m_initialized = false;
+}
+
+void CFrmChineseChessPlugin::initialize(QDesignerFormEditorInterface * /* core */)
+{
+    if (m_initialized)
+        return;
+    
+    // Add extension registrations, etc. here
+    
+    m_initialized = true;
+}
+
+bool CFrmChineseChessPlugin::isInitialized() const
+{
+    return m_initialized;
+}
+
+QWidget *CFrmChineseChessPlugin::createWidget(QWidget *parent)
+{
+    return new CFrmChineseChess(parent);
+}
+
+QString CFrmChineseChessPlugin::name() const
+{
+    return tr("Chinese chess");
+}
+
+QString CFrmChineseChessPlugin::group() const
+{
+    return QLatin1String("");
+}
+
+QIcon CFrmChineseChessPlugin::icon() const
+{
+    return QIcon(QLatin1String(":/Chess"));
+}
+
+QString CFrmChineseChessPlugin::toolTip() const
+{
+    return tr("Chinese chess");
+}
+
+QString CFrmChineseChessPlugin::whatsThis() const
+{
+    return tr("Chinese chess");
+}
+
+bool CFrmChineseChessPlugin::isContainer() const
+{
+    return false;
+}
+
+QString CFrmChineseChessPlugin::domXml() const
+{
+    return QLatin1String("<widget class=\"CFrmChineseChess\" name=\"frmChineseChess\">\n</widget>\n");
+}
+
+QString CFrmChineseChessPlugin::includeFile() const
+{
+    return QLatin1String("FrmChineseChess.h");
+}
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN2(FrmChineseChessPlugin, CFrmChineseChessPlugin)
+#endif // QT_VERSION < 0x050000
---
+++ Src/Qt/FrmChineseChessPlugin.h
@@ -0,0 +1,33 @@
+#ifndef FRMCHINESECHESSPLUGIN_H
+#define FRMCHINESECHESSPLUGIN_H
+
+#include <QDesignerCustomWidgetInterface>
+
+class CFrmChineseChessPlugin : public QObject, public QDesignerCustomWidgetInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QDesignerCustomWidgetInterface")
+#endif // QT_VERSION >= 0x050000
+    
+public:
+    CFrmChineseChessPlugin(QObject *parent = nullptr);
+    
+    bool isContainer() const;
+    bool isInitialized() const;
+    QIcon icon() const;
+    QString domXml() const;
+    QString group() const;
+    QString includeFile() const;
+    QString name() const;
+    QString toolTip() const;
+    QString whatsThis() const;
+    QWidget *createWidget(QWidget *parent);
+    void initialize(QDesignerFormEditorInterface *core);
+    
+private:
+    bool m_initialized;
+};
+
+#endif // FRMCHINESECHESSPLUGIN_H
