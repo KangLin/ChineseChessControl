375ecd5f05d63d13894e391532e0aa11387e2c29
Kang Lin <kl222@126.com>
2021-12-16 09:23:50 +0800

    Add translations

--- App/Qt/ChineseChess/Resource/Translations/ChineseChessApp_sl.ts
+++ App/Qt/ChineseChess/Resource/Translations/ChineseChessApp_sl.ts
@@ -1,89 +1,89 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_TW">
+<TS version="2.1" language="sl_SI">
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../../MainWindow.ui" line="14"/>
+        <location filename="../../MainWindow.cpp" line="71"/>
+        <source>Chinese chess</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../../MainWindow.ui" line="32"/>
         <source>File(&amp;F)</source>
-        <translation>文件(&amp;F)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="40"/>
         <source>Tools(&amp;T)</source>
-        <translation>工具(&amp;T)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="49"/>
         <source>Help(&amp;H)</source>
-        <translation>幫助(&amp;H)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="61"/>
         <source>toolBar</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="82"/>
         <source>Open(&amp;O)</source>
-        <translation>打開(&amp;O)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="91"/>
         <source>Save(&amp;S)</source>
-        <translation>保存(&amp;S)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="100"/>
         <source>Previou(&amp;P)</source>
-        <translation>上壹步(&amp;P)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="109"/>
         <source>Next(&amp;N)</source>
-        <translation>下壹步(&amp;N)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="118"/>
         <source>About(&amp;A)</source>
-        <translation>關於(&amp;A)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="123"/>
         <source>Update(&amp;U)</source>
-        <translation>更新(&amp;U)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="132"/>
         <source>Exit(&amp;E)</source>
-        <translation>退出(&amp;E)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="140"/>
         <source>Prompt message(&amp;M)</source>
-        <translation>提示消息(&amp;M)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.ui" line="148"/>
         <source>Prompt sound(&amp;S)</source>
-        <translation>提示音(&amp;S)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.cpp" line="35"/>
         <location filename="../../MainWindow.cpp" line="44"/>
         <source>Open chess game</source>
-        <translation>打開棋局</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../MainWindow.cpp" line="46"/>
         <source>Chinese chess control file(*.ccc);;Portable game notation file(*.pgn);;All files(*.*)</source>
         <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../MainWindow.ui" line="14"/>
-        <location filename="../../MainWindow.cpp" line="71"/>
-        <source>Chinese chess</source>
-        <translation>中國象棋</translation>
     </message>
 </context>
 </TS>
--- UI/Qt/Resource/Translations/ChineseChessQt_sk.ts
+++ UI/Qt/Resource/Translations/ChineseChessQt_sk.ts
@@ -1,33 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="sk_SK">
 <context>
     <name>CFrmChineseChess</name>
     <message>
         <location filename="../../FrmChineseChess.ui" line="14"/>
         <source>Chinese chess</source>
-        <translation>中国象棋</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../FrmChineseChess.cpp" line="157"/>
         <location filename="../../FrmChineseChess.cpp" line="161"/>
         <source>This step cannot be taken, otherwise you will lose</source>
-        <translation>这步不能走，否则会输棋！</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../FrmChineseChess.cpp" line="158"/>
         <source>checked</source>
-        <translation>被将军</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../FrmChineseChess.cpp" line="162"/>
         <source>Opposite</source>
-        <translation>将对面</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../FrmChineseChess.cpp" line="198"/>
         <source>Chinese chess control</source>
-        <translation>中国象棋控件</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
         <location filename="../../FrmChineseChessPlugin.cpp" line="50"/>
         <location filename="../../FrmChineseChessPlugin.cpp" line="55"/>
         <source>Chinese chess</source>
-        <translation>中国象棋</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
