056b4f6eaa072a834d0af54a842b3fcf81ddbc52
Kang Lin <kl222@126.com>
2026-06-26 15:22:02 +0800

    App: modify qt application main.c

--- App/Qt/ChineseChess/main.cpp
+++ App/Qt/ChineseChess/main.cpp
@@ -1,41 +1,76 @@
-#include "MainWindow.h"
+// Author: Kang Lin <kl222@126.com>
+
+#include <QApplication>
+#include <QLoggingCategory>
+#include <QTranslator>
+
 #include "FrmChineseChess.h"
 #include "RabbitCommonDir.h"
 #include "RabbitCommonTools.h"
 #include "FrmUpdater/FrmUpdater.h"
 
-#include <QApplication>
-#include <QDebug>
-#include <QTranslator>
+#include "MainWindow.h"
 
+static Q_LOGGING_CATEGORY(log, "App.Main")
 int main(int argc, char *argv[])
 {
     int nRet = 0;
-    QApplication a(argc, argv);
+#ifdef ChineseChessApp_VERSION
+    QApplication::setApplicationVersion(ChineseChessApp_VERSION);
+#endif
+    QApplication::setApplicationName("ChineseChessControl");
+    QApplication app(argc, argv);
 
-    a.setApplicationName("ChineseChessControl");
-
-    QString szLoacleName = QLocale::system().name();
-    RabbitCommon::CTools::Instance()->Init(szLoacleName);
-    CFrmChineseChess::InitResource(szLoacleName);
+    RabbitCommon::CTools::Instance()->Init();
+    CFrmChineseChess::InitResource(RabbitCommon::CTools::Instance()->GetLanguage());
 
     QSharedPointer<QTranslator> translator =
         RabbitCommon::CTools::Instance()->InstallTranslator("ChineseChessApp");
 
-    a.setApplicationDisplayName(QObject::tr("Chinese chess"));
+    app.setApplicationDisplayName(QObject::tr("Chinese chess"));
 
-    CFrmUpdater *pUpdate = new CFrmUpdater();
-    pUpdate->SetTitle(QImage(":/image/Chess"));
-    if(a.arguments().length() > 1) {
-        pUpdate->GenerateUpdateJson();
-        pUpdate->GenerateUpdateXml();
-        return 0;
+#ifdef HAVE_UPDATE
+    // Check update version
+    QScopedPointer<CFrmUpdater> pUpdater(new CFrmUpdater());
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    if(qEnvironmentVariable("SNAP").isEmpty()
+        && qEnvironmentVariable("FLATPAK_ID").isEmpty())
+#else
+    if(qgetenv("SNAP").isEmpty()
+        && qgetenv("FLATPAK_ID").isEmpty())
+#endif
+    {
+        if(pUpdater) {
+            pUpdater->setAttribute(Qt::WA_DeleteOnClose, false);
+            QIcon icon = QIcon::fromTheme(":/image/Chess");
+            if(!icon.isNull()) {
+                auto sizeList = icon.availableSizes();
+                if(!sizeList.isEmpty()) {
+                    QPixmap p = icon.pixmap(*sizeList.begin());
+                    pUpdater->SetTitle(p.toImage());
+                }
+            }
+            if(app.arguments().length() > 1) {
+                try {
+                    pUpdater->GenerateUpdateJson();
+                    pUpdater->GenerateUpdateXml();
+                } catch(...) {
+                    qCritical(log) << "Generate update fail";
+                }
+                qInfo(log) << app.applicationName() + " " + app.applicationVersion()
+                                  + " " + QObject::tr("Generate update json file End");
+                return 0;
+            }
+        } else {
+            qCritical(log) << "new CFrmUpdater() fail";
+        }
     }
+#endif // #ifdef HAVE_UPDATE
 
     MainWindow w;
-    w.show();
+    RC_SHOW_WINDOW(&w);
 
-    nRet = a.exec();
+    nRet = app.exec();
 
     if(translator)
         RabbitCommon::CTools::Instance()->RemoveTranslator(translator);
